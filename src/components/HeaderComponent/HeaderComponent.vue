517d7ec602fd3ba11aa6a9ec7c5ee9e2fa5b5207
Shubham Kamath <dev.shubhaem@gmail.com>
2020-08-17 02:12:53 +0530

    feat: added more menu grid

--- src/components/HeaderComponent/HeaderComponent.vue
+++ src/components/HeaderComponent/HeaderComponent.vue
@@ -4,30 +4,43 @@
       <MaxWidthComponent class="nav-container-child">
         <div class="nav-item">
           <span class="angle-bracket" v-html="'<'" />
-          <span class="title-text">{{titleText}}</span>
+          <span
+            class="title-text md:title-text lg:title-text xl:title-text 4k:title-text"
+            >{{ titleText }}</span
+          >
           <span class="front-slash" v-html="'/'" />
           <span class="angle-bracket" v-html="'>'" />
         </div>
         <div class="nav-item" @click="$emit('toggle-menu')">
           <span class="angle-bracket" v-html="'<'" />
-          <span class="title-text">{{menuText}}</span>
+          <span
+            class="title-text md:title-text lg:title-text xl:title-text 4k:title-text"
+            >{{ menuText }}</span
+          >
           <span class="front-slash" v-html="'/'" />
           <span class="angle-bracket" v-html="'>'" />
         </div>
       </MaxWidthComponent>
       <MaxWidthComponent class="nav-container-child" v-if="isMenuOpen">
-        <div class="h-screen w-screen flex">
-          <div class="h-full w-1/2 bg-teal-200" />
-          <div class="h-full w-1/2 bg-teal-400 flex flex-col relative">
-            <div class="h-auto min-h-1/2 w-full bg-red-500" />
-            <div class="h-auto min-h-1/2 w-full bg-red-600" />
+        <div class="h-screen w-screen flex flex-col-reverse md:flex-row">
+          <div class="h-full w-full bg-teal-200 md:h-full md:w-1/2" />
+          <div
+            class="h-full w-full bg-teal-400 flex flex-col relative md:h-full md:w-1/2"
+          >
+            <div class="h-auto min-h-1/2 w-full bg-red-500 flex">
+              <div class="h-full w-1/2 bg-teal-400 flex flex-col relative" />
+              <div class="h-full w-1/2 bg-teal-700 flex flex-col relative">
+                <div class="h-auto min-h-1/2 w-full bg-red-600"></div>
+                <div class="h-auto min-h-1/2 w-full bg-red-900"></div>
+              </div>
+            </div>
+            <div class="h-auto min-h-1/2 w-full bg-red-600"></div>
           </div>
         </div>
       </MaxWidthComponent>
     </nav>
   </transition>
 </template>
-
 
 <script>
 import MaxWidthComponent from "../MaxWidthComponent/MaxWidthComponent";
@@ -46,7 +59,7 @@ export default {
       SCROLL_POSITION: (state) => state.appBehavior.SCROLL_POSITION,
     }),
   },
-  data: function () {
+  data: function() {
     return {
       titleText: TITLE_TEXT,
     };
