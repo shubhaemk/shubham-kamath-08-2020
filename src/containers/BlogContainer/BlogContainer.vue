0aa61334d4c2a0b21a3451e8c8ee8fa4799e8b3d
Shubham Kamath <dev.shubhaem@gmail.com>
2020-09-01 00:23:02 +0530

    feat: minor styling changes

--- src/containers/BlogContainer/BlogContainer.vue
+++ src/containers/BlogContainer/BlogContainer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h-screen min-h-50 px-48 relative">
-    <div class="w-32 h-full flex justify-center">
+  <div class="h-screen min-h-50 relative">
+    <div class="h-full flex justify-center absolute left-0">
       <div class="w-1 h-full bg-blog" />
     </div>
   </div>
