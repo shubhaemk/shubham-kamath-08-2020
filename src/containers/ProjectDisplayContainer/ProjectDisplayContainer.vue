bb218e18bd890046a8623182373cbdc65bc4c639
Shubham Kamath <dev.shubhaem@gmail.com>
2020-09-27 04:26:04 +0530

    feat: basic wireframe for project

--- src/containers/ProjectDisplayContainer/ProjectDisplayContainer.vue
+++ src/containers/ProjectDisplayContainer/ProjectDisplayContainer.vue
@@ -2,9 +2,25 @@
   <div class="h-full min-h-50 relative" :screen-name="screenName">
     <span class="absolute left-0 bottom-0 h-full w-2 bg-project" />
     <div class="px-20 flex flex-col">
-      <div class="w-full h-50vh fourk:h-30vh fourk:h-30vh -mt-20 flex mb-20">
+      <div
+        class="w-full h-50vh fourk:h-30vh fourk:h-30vh -mt-20 flex mb-20 relative"
+      >
+        <div class="absolute top-0 px-8 py-5 bg-project w-1/2 bg-opacity-25">
+          <span class="text-white text-4xl">Web</span>
+        </div>
         <img class="h-full w-1/2" src="../../assets/Test.jpg" />
-        <div class="h-full w-1/2 bg-white" />
+        <div class="h-full w-1/2 bg-project flex flex-col py-10 px-16">
+          <div class="flex justify-between mb-10">
+            <span class="text-5xl font-semibold text-white text-left"
+              >GitCard</span
+            >
+            <span class="text-5xl font-semibold text-white text-left">00</span>
+          </div>
+
+          <span class="text-2xl font-medium text-gray-400 mb-5 text-left"
+            >Generate GitHub Profile Cards, and download it to share.</span
+          >
+        </div>
       </div>
       <div class="w-full h-50vh fourk:h-30vh flex justify-between mb-20">
         <div class="h-full w-1/2 bg-white" />
@@ -18,9 +34,17 @@
         <div class="h-full w-1/2 bg-white" />
         <img class="h-full w-1/2" src="../../assets/Test-4.jpg" />
       </div>
-      <div class="w-full h-50vh fourk:h-30vh flex justify-between mb-20">
+      <div class="w-full h-50vh fourk:h-30vh flex justify-between">
         <img class="h-full w-1/2" src="../../assets/Test-5.jpg" />
         <div class="h-full w-1/2 bg-white" />
+      </div>
+      <div class="py-20 flex justify-end">
+        <span class="text-4-5xl text-gray-300 font-hairline mr-5"
+          >Check more on my
+        </span>
+        <span class="text-4-5xl text-gray-300 font-hairline cursor-pointer"
+          >GitHub &rarr;</span
+        >
       </div>
     </div>
   </div>
