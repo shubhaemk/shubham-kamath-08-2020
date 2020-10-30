a86881e36758e0a5802a22ff833c6d3281f44bf8
Shubham Kamath <dev.shubhaem@gmail.com>
2020-10-30 20:16:23 +0530

    feat: blog introduction

--- src/containers/BlogContainer/BlogContainer.vue
+++ src/containers/BlogContainer/BlogContainer.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="h-screen min-h-50 relative" :screen-name="screenName">
-    <div class="h-full flex justify-center absolute left-0">
-      <div class="w-1 h-full bg-blog" />
+    <div class="h-full w-full relative">
+      <span class="absolute left-0 bottom-0 h-full w-2 bg-blog" />
+      <div class="absolute left-0 top-15per flex flex-col w-full">
+        <span
+          class="px-20 py-10 w-fit-content bg-blog text-6xl text-white font-medium"
+          >Blogs</span
+        >
+        <div class="px-20 flex flex-col">
+          <span class="py-10">
+            <span class="text-6xl text-white font-hairline">
+              I write about things I want to discover more about.
+            </span>
+          </span>
+          <span class="py-5">
+            <span class="text-4-5xl text-gray-300 font-hairline">
+              Dev.to and Twitter is where I write. <span class="text-4-5xl text-blog font-hairline">¯\_(ツ)_/¯</span>
+            </span>
+          </span>
+          <span class="py-5">
+            <span class="text-4-5xl text-gray-300 font-hairline">
+              Read my Blogs Below.
+            </span>
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
