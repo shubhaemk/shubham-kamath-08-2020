c574791d72363ff20097312b230f20e53f13c75a
Shubham Kamath <dev.shubhaem@gmail.com>
2020-11-01 01:47:45 +0530

    feat: completed contact component

--- src/containers/ContactContainer/ContactContainer.vue
+++ src/containers/ContactContainer/ContactContainer.vue
@@ -1,38 +1,99 @@
 <template>
   <div class="h-screen min-h-50 relative" :screen-name="screenName">
     <div class="h-full w-full relative">
-      <span class="absolute left-0 bottom-0 h-full w-2 bg-contact" />
+      <span class="absolute left-0 top-0 h-full w-2 bg-contact" />
       <div class="absolute left-0 top-15per flex flex-col w-full">
         <span
           class="px-20 py-10 w-fit-content bg-contact text-6xl text-white font-medium"
         >
           Contact
         </span>
-        <div class="px-20 flex flex-col">
-          <span class="text-4-5xl py-10 text-gray-300 font-hairline">
-            Hi, I am Shubham Kamath born and brought up in Pune, India and
-            currently working in Bengaluru, India. I have a Masters Degree in
-            Computer Science and specializes in Frontend Technologies.
+        <div class="px-20 py-10 flex flex-wrap justify-between">
+          <span class="py-10 w-full">
+            <span class="text-6xl text-white font-hairline">
+              Let's connect if you have any oportunity or just wanna say Hi!
+            </span>
           </span>
-          <span class="text-4-5xl py-5 text-gray-300 font-hairline">
-            Besides developing Web Apps, I'm a hobbiest photographer, clicking
-            subjects I find intriguing,
-          </span>
-          <span class="text-4-5xl py-5 text-gray-300 font-hairline">
-            I leverage my visual skills to create better looking softwares.
-          </span>
-          <span class="text-4-5xl py-5 text-gray-300 font-hairline">
-            Connect with me if you have any opportunity or just want to say Hi
-            &darr;
-          </span>
+          <div class="w-45per contact-container">
+            <ContactIconComponent
+              title="Mail"
+              url="mailto:shubhamkamaths@gmail.com"
+              description="shubhamkamaths@gmail.com"
+            >
+              <MailIcon class="w-1/2 contact-icon" />
+            </ContactIconComponent>
+          </div>
+          <div class="w-45per contact-container">
+            <ContactIconComponent
+              title="LinkedIn"
+              url="https://www.linkedin.com/in/shubham-kamath"
+              description="linkedin.com/in/shubham-kamath"
+            >
+              <LinkedinIcon class="w-1/2 contact-icon" />
+            </ContactIconComponent>
+          </div>
+          <div class="w-45per contact-container">
+            <ContactIconComponent
+              title="Twitter"
+              url="https://www.twitter.com/shubham_kamath"
+              description="twitter.com/shubham_kamath"
+            >
+              <TwitterIcon class="w-1/2 contact-icon" />
+            </ContactIconComponent>
+          </div>
+          <div class="w-45per contact-container">
+            <ContactIconComponent
+              title="Instagram"
+              url="https://www.instagram.com/shubham_kamath"
+              description="instagram.com/shubham_kamath"
+            >
+              <InstagramIcon class="w-1/2 contact-icon" />
+            </ContactIconComponent>
+          </div>
+          <div class="w-45per contact-container">
+            <ContactIconComponent
+              title="GitHub"
+              url="https://github.com/shubhaemk"
+              description="github.com/shubhaemk"
+            >
+              <GitHubIcon class="w-1/2 contact-icon" />
+            </ContactIconComponent>
+          </div>
+          <div class="w-45per contact-container">
+            <ContactIconComponent
+              title="Dev.to"
+              url="https://dev.to/shubhamk"
+              description="dev.to/shubhamk"
+            >
+              <DevToIcon class="w-1/2 contact-icon" />
+            </ContactIconComponent>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
+import ContactIconComponent from "../../components/CotactIconComponent/ContactIconComponent";
+import MailIcon from "../../assets/email.svg";
+import LinkedinIcon from "../../assets/linkedin.svg";
+import TwitterIcon from "../../assets/twitter.svg";
+import InstagramIcon from "../../assets/instagram.svg";
+import GitHubIcon from "../../assets/github.svg";
+import DevToIcon from '../../assets/devto.svg';
+
 export default {
   name: "ContactContainer",
+  components: {
+    ContactIconComponent,
+    MailIcon,
+    LinkedinIcon,
+    TwitterIcon,
+    InstagramIcon,
+    GitHubIcon,
+    DevToIcon
+  },
   mounted() {
     this.observer.observe(this.$el);
   },
@@ -54,4 +115,6 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import "./ContactContainer.css";
+</style>
